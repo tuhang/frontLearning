5252d366e871ae51d18c0777b7c05954716f5d82
Henry <tu_hang@126.com>
2023-11-20 08:40:54 +0800

    feat:基本js语法

--- day1_HTMLAndCSS/test.js
+++ day1_HTMLAndCSS/test.js
@@ -1,2 +1,23 @@
-console.log(a);
 var a = 1;
+console.log(a);
+
+var obj ={
+    foo:"foo",
+    bar:"bar"
+}
+
+var obj2 = {
+    p: function (x) {
+      return 2 * x;
+    }
+  };
+
+
+console.log(obj);
+console.log(obj2.p(1));
+
+function print2(s){
+  console.log(s)
+}
+
+print2(3)
